dc0f1ba3c79161484f49878715218e8c9f3ea7f3
bezerradsb <danilo.sbezerra@gmail.com>
2021-12-10 23:02:00 -0300

    Aula 10: salvando um dado de uma entidade no banco relacionado a outra entidade

--- eventoapp/eventoapp/src/main/java/com/eventoapp/eventoapp/models/Convidado.java
+++ eventoapp/eventoapp/src/main/java/com/eventoapp/eventoapp/models/Convidado.java
@@ -8,34 +8,35 @@ import javax.persistence.ManyToOne;
 public class Convidado {
 
 	@Id
-	private String rg;
+	private long rg;
 	private String nomeConvidado;
 	
 	@ManyToOne
 	private Evento evento;
-	
-	public String getRg() {
+
+	public long getRg() {
 		return rg;
 	}
-	
-	public void setRg(String rg) {
+
+	public void setRg(long rg) {
 		this.rg = rg;
 	}
-	
+
 	public String getNomeConvidado() {
 		return nomeConvidado;
 	}
-	
+
 	public void setNomeConvidado(String nomeConvidado) {
 		this.nomeConvidado = nomeConvidado;
 	}
-	
+
 	public Evento getEvento() {
 		return evento;
 	}
-	
+
 	public void setEvento(Evento evento) {
 		this.evento = evento;
 	}
 	
+
 }
